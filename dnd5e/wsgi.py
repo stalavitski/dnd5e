caa462e91fdf7fd00c25bfe52f060a6ea4451638
Artsem Stalavitski <a.stalavitski@gmail.com>
2020-02-20 19:50:54 +0300

    init - reorganized project structure

--- dnd5e/wsgi.py
+++ dnd5e/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dnd.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dnd5e.settings')
 
 application = get_wsgi_application()
